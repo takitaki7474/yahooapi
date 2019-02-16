44c7e36f012d49019ac1876d2a52bc2f2e49362e
oota <shark747474@gmail.com>
2019-02-16 15:20:44 +0900

    yahooapisample

--- sample2/main.mjs
+++ sample2/main.mjs
@@ -25,7 +25,7 @@
  */
 async function requestGeoCoderAPI(query) {
   const response = await axios({
-    url: 'https://{あなたのApigeeURL}/geocode/V1/geoCoder',
+    url: 'https://shark747474-eval-test.apigee.net/yahoo_map/js/V1/jsapi/geocode/V1/geoCoder',
     params: {
       query,
       output: 'json',
